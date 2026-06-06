0227ec6a33446f838a2e3d0003fe106fed8205ed
K4LM3D <126100750+k4lm3d@users.noreply.github.com>
2026-06-07 02:50:05 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('launch-btn').addEventListener('click', function(e) {
+    // Just a cool little console Easter egg for players who inspect element
+    console.log("🔓 [ACCESS GRANTED]: Connecting to Lovely Loren's device...");
+    
+    // Optional: You can add an audio beep here if you host an audio file!
+});
